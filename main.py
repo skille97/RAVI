81e11897a3e71aff2c59284f2b35b43b78d579f5
laurits <laurits.randers@gmail.com>
2016-06-09 20:59:49 +0200

    har state på databasen

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 #!/usr/bin/env python3
 from flask import *
+import sqlite3
 app = Flask(__name__)
-
-
 
 
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
 
-
+def dbinet():
+    db = sqlite3.connect("RAVI.db")
+    db.execute("""CREATE TABLE opgaver (
+                id int,
+                name text
+                )
+               """)
 
 @app.route("/")
 def main():
@@ -41,6 +46,7 @@ def addRow():
 
 
 if __name__ == "__main__":
+    dbinet()
     app.run(debug=True, host="0.0.0.0")
 
 
