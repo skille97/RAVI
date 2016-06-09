cca3e3b17241bbc6c6282a5abde375f9ef35f449
benjadahl <benjadahl>
2016-06-09 20:40:32 +0200

    Startet conversion to using <input> tags instead of prompt

--- static/index.js
+++ static/index.js
@@ -1,6 +1,14 @@
 $(document).ready(function(){
     $(".data").click(function(){
-      var response = prompt("Enter value for cell:","Value")
-      $(this).html(response);
+      $(this).replaceWith("<input type='text'></input>");
+
+    });
+
+    $(window).keydown(function(key){
+      if(key.key === "Enter"){
+        if(document.getElementsByTagName("input").length > 0){
+          console.log(document.getElementsByTagName("input"));
+        }
+      }
     });
 });
