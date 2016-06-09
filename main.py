0d37934832f40166614a7201db7c92f4ca183110
Rasmus Rendal <RasmusRendal@gmail.com>
2016-06-09 14:30:01 +0200

    Added table

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return render_template('index.html')
+    headers = ["Header1", "Header2", "Header3"]
+    body = [["Body", "Body", "Body"], ["Body", "Body", "Body"]]
+    return render_template('index.html', headers=headers, body=body)
 
 if __name__ == "__main__":
     app.run(debug=True)
