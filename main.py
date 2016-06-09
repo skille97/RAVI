01344b4361ede9492bdf2240f23cadf958f7c9bb
Julian <jbjjbjjbj>
2016-06-09 14:29:54 +0200

    Added post request

--- main.py
+++ main.py
@@ -3,11 +3,26 @@ app = Flask(__name__)
 
 
 
-
-
 @app.route("/")
 def main():
     return render_template('index.html')
+
+@app.route('/changeCell/', methods=['POST'])
+def changeCell():
+    cell=request.form['cell']
+    data=request.form['data']
+
+
+@app.route('/moveRow/', methods=['POST'])
+def moveRow():
+    axis=request.form['axis']
+    start=request.form['from']
+    to=request.form['to']
+
+    
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
