ea8e108344a18613d3ea5c075da881ec528160d5
Julian <jbjjbjjbj>
2016-06-09 16:10:12 +0200

    Added danish 404 page

--- main.py
+++ main.py
@@ -3,6 +3,13 @@ app = Flask(__name__)
 
 
 
+
+@app.errorhandler(404)
+def page_not_found(e):
+        return render_template('404.html'), 404
+
+
+
 @app.route("/")
 def main():
     headers = ["Header1", "Header2", "Header3"]
@@ -13,6 +20,7 @@ def main():
 def changeCell():
     cell=request.form['cell']
     data=request.form['data']
+    return 1
 
 
 @app.route('/moveRow/', methods=['POST'])
@@ -20,12 +28,21 @@ def moveRow():
     axis=request.form['axis']
     start=request.form['from']
     to=request.form['to']
+    return 1
 
     
-
+@app.route('/addRow/', methods=['POST'])
+def addRow():
+    axis=request.form['axis']
+    number=request.form['number']
+    return 1
 
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0")
+
+
+
+
 
