f2cdd5c5b5e348a24487f62f2f6697bd901e6b67
benjadahl <benjadahl>
2016-06-10 18:37:20 +0200

    Made progress on the input fields

--- static/index.js
+++ static/index.js
@@ -1,7 +1,8 @@
 $(document).ready(function(){
     $(".data").click(function(){
       if(document.getElementsByTagName("input").length > 0){
-        $("#" + document.getElementsByTagName("input")[0].id).replaceWith("<td>test</td>")
+          $("#" + document.getElementsByTagName("input")[0].id).replaceWith("<td>test</td>")
+          console.log(document.getElementsByTagName("input")[0])
       }
       $(this).replaceWith("<input id='" + $(this).attr("id") + "' type='text' value='" + $(this).html() + "'></input>");
     });
