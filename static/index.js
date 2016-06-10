63b57a96a39a774a88b924c97b58fc01617beb2e
benjadahl <benjadahl>
2016-06-10 22:44:18 +0200

    Added window.click function

--- static/index.js
+++ static/index.js
@@ -1,26 +1,23 @@
 $(document).ready(function(){
-	function closeInput () {
-		if(document.getElementsByTagName("input").length > 0){
-			var inputElement = document.getElementsByTagName("input")[0];
-			$("#" + inputElement.id).replaceWith("<td class='data' id='" + inputElement.id +"'>" + inputElement.value + "</td>")
-		}
-	}
+  function closeInput () {
+    if(document.getElementsByTagName("input").length > 0){
+        var inputElement = document.getElementsByTagName("input")[0];
+        $("#" + inputElement.id).replaceWith("<td class='data' id='" + inputElement.id +"'>" + inputElement.value + "</td>")
+    }
+  }
 
-	$(document).on("click", ".data", function(){
-		closeInput();
-		$(this).replaceWith("<input id='" + $(this).attr("id") + "' type='text' value='" + $(this).html() + "'></input>");
-		console.log("test");
-	});
+  $(document).on("click", ".data", function(){
+    closeInput();
+    $(this).replaceWith("<input id='" + $(this).attr("id") + "' type='text' value='" + $(this).html() + "'></input>");
+  });
 
-	$(window).keydown(function(key){
-		if(key.key === "Enter"){
-			closeInput();
-		}
-	});
+  $(window).keydown(function(key){
+    if(key.key === "Enter"){
+      closeInput();
+    }
+  });
+
+  $(window).click(function(){
+    console.log("Dank")
+  });
 });
-
-
-window.setInterval(function(){
-	$("#table").load(document.URL + ' #table');
-	console.log("Reloading table");
-}, 5000);
