3e25a5d54ebf55c7d805526d05cf53affa18e826
benjadahl <benjadahl@gmail.com>
2016-07-03 13:12:58 +0200

    Removed the files that are not necesary for realease

--- main.py
+++ main.py
@@ -4,24 +4,9 @@ import sqlite3
 import re
 import atexit
 
-#BOM stuff
-import os
-from werkzeug.utils import secure_filename
-from BOM.bom import conveter
-import shutil
-
-#for tempfiles
-UPLOAD_FOLDER = './upload/'
-
-
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 db_name = "RAVI.db"
-
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template('404.html'), 404
 
 #TODO: Database versioning
 def dbInit():
@@ -137,41 +122,6 @@ def updateColour():
     db.commit()
     db.close()
     return "true"
-
-
-
-
-@app.route('/bom/', methods=['GET', 'POST'])
-def upload_file():
-    #Check if there is a request
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        # if user does not select file, browser also
-        # submit a empty part without filename
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file:
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
-            #Run the converter
-            conveter(UPLOAD_FOLDER + file.filename);
-            #Send converted file back to user
-            return send_file(UPLOAD_FOLDER + file.filename + ".csv", as_attachment=True)
-    #If no file is uploaded send bom html upload site
-    return render_template('bom.html')
-
-
-#Remove tempfiles on exit
-@atexit.register
-def exit():
-    print("Removing temp files")
-    shutil.rmtree(UPLOAD_FOLDER)
-    os.makedirs(UPLOAD_FOLDER)
-
 
 
 if __name__ == "__main__":
